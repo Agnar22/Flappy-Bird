b6438270cd6f514285214c6b20ddfebb4e38ff75
Agnar22 <agnar.bjornstad@gmail.com>
2019-09-03 18:19:24 +0200

    added main

---
+++ Main.py
@@ -0,0 +1,12 @@
+# TODO:
+#       put wrapper class player in separate file
+#       gamerendering calls gamelogic (opposite today)
+#       cleanup gamerendering
+#       make it possible to adjust and maximize fps
+#       add soundeffects on time
+#       cleanup gamelogic
+#       DQN -> Double Duelling DQN with prioritized experience replay
+#       cleanup DQN.py
+#       make it possible to sort NEAT-agents and return one of them
+#       cleanup NEATagent.py
+#       run everything from main (no executing code in other files
